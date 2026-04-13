20f3dc0e587d9b34553857e91823fa16a3ce1885
Thando Moeketsi Ndlovu <45040537+thando-ndlovu@users.noreply.github.com>
2026-04-14 01:51:14 +0200

    protobuf use changed from 'hedera-protobufs => 'hiero-consensus-node''

--- src/Hedera.Hashgraph.Tests.Integration/obj/Debug/net10.0/Hedera.Hashgraph.Tests.Integration.AssemblyInfo.cs
+++ src/Hedera.Hashgraph.Tests.Integration/obj/Debug/net10.0/Hedera.Hashgraph.Tests.Integration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee27b57808fd598e13573cb5209bfcf24dadbff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191b3bf9df8d2b099b6aa2c8c7f0b958eef62389")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
