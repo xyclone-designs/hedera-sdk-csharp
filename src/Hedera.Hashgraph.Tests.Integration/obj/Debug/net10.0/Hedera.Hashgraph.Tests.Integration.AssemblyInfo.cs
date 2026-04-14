071b591f936a3731b9854a84a10d4e49fb8bdd23
Thando Moeketsi Ndlovu <45040537+thando-ndlovu@users.noreply.github.com>
2026-04-14 22:58:46 +0200

    change fork name

--- src/Hedera.Hashgraph.Tests.Integration/obj/Debug/net10.0/Hedera.Hashgraph.Tests.Integration.AssemblyInfo.cs
+++ src/Hedera.Hashgraph.Tests.Integration/obj/Debug/net10.0/Hedera.Hashgraph.Tests.Integration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607afa9e37ad23e087aca7e69d3d094b4f862d39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dd46fd376ffae5b72615e17f253ff8e5b8508b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hedera.Hashgraph.Tests.Integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
